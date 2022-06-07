7252ce3e79578ae0d6b11c88f8a3ea5e1062cded
Alexandre Vaast <alex.vaast@gmail.com>
2022-06-07 16:46:24 +0200

    remove useless api auth

--- src/api/http-common.js
+++ src/api/http-common.js
@@ -5,9 +5,5 @@ export const HTTP = axios.create({
   headers: {
     "Content-Type": "application/hal+json",
     Accept: "application/vnd.api+json"
-  },
-  auth: {
-    username: process.env.VUE_APP_ENDPOINT_NAME,
-    password: process.env.VUE_APP_ENDPOINT_PASSWORD
   }
 });
